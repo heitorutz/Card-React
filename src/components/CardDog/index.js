5245fb5f60244850ba16c34ceaa99b41463d9070
Heitor Urbanetz <jhty123453@gmail.com>
2022-07-20 12:51:46 -0300

    Loading

--- src/components/CardDog/index.js
+++ src/components/CardDog/index.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import Loading from '../../utils/loading.gif';
 
 const CarDog = ({ name, image, index }) => {
 
     const randomNumber = Math.floor((Math.random() * 10) + 1);
 
+    useEffect(() => {
+        const loading = document.querySelector('.card-dog-loading');
+        setTimeout(() => {
+            loading.style.display = 'none';
+        }, 3000);
+    }, []);
+
   return (
     <div className='card-dog'>
         <h1 className='card-dog-name'>{name} { randomNumber } </h1>
+        
+        
+        <img className='card-dog-loading' src={Loading} alt="loading"/>
         
         <img className='card-dog-image' src={image[index]} alt={name}/>
     </div>
