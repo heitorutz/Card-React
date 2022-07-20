83a9387f8f050e29433f9100c0064569b4415e79
Heitor Urbanetz <jhty123453@gmail.com>
2022-07-20 12:31:35 -0300

    Fetch Imagens

--- src/Card.js
+++ src/Card.js
@@ -4,6 +4,7 @@ import CarDog from "./components/CardDog";
 const Card = () => {
   const [ dogName, setDogName ] = useState([]);
   const [ dogs, setDogs ] = useState([]);
+  const [ dogImages, setDogImages ] = useState([]);
 
   useEffect(() => {
     const getNameDogs = async () => {
@@ -16,10 +17,25 @@ const Card = () => {
   }, []);
 
   useEffect(() => {
-    const randomDogs = Math.floor((Math.random() * 96) + 1);
+    const randomDogs = Math.floor((Math.random() * 80) + 1);
     const onlyFiveDogs = Object.entries(dogName).slice(randomDogs, randomDogs + 5);
-    onlyFiveDogs.map(el => setDogs(currentList => [...currentList, el[0]]));
+    onlyFiveDogs.map(el => {
+
+      
+      const getImageDog = async () => {
+        const imageDog = `https://dog.ceo/api/breed/${el[0]}/images`;
+        const data = await fetch(imageDog);
+        const json = await data.json();
+        console.log(json.message[0]);
+        setDogImages(currentImages => [...currentImages, json.message[0]]);
+    };
+
+      getImageDog();
+      return setDogs(currentList => [...currentList, el[0]]);
+    });
+  
   }, [dogName]);
+
     
   return (
     <div className="Card">
