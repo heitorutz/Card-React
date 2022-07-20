ebffd0e69d02b6fe880e0357b056a2cde396503f
Heitor Urbanetz <jhty123453@gmail.com>
2022-07-20 11:34:45 -0300

    Pegar apenas 5 cachorros

--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,35 @@
-function Card() {
+import { useEffect, useState } from "react";
+
+const Card = () => {
+  const [ dogName, setDogName ] = useState([]);
+  const [ dogs, setDogs ] = useState([]);
+
+  useEffect(() => {
+    const getNameDogs = async () => {
+        const data = await fetch('https://dog.ceo/api/breeds/list/all');
+        const json = await data.json();
+        setDogName(json.message);
+    };
+
+    getNameDogs();
+  }, []);
+
+  useEffect(() => {
+    const onlyFiveDogs = Object.entries(dogName).slice(0, 5);
+    onlyFiveDogs.map(el => setDogs(currentList => [...currentList, el[0]]));
+  }, [dogName]);
+    
   return (
-    <div className="Home">
-        <div className="home-input">
-          <h3>JOGO</h3>
-          <input type="text"/>
-        </div>
+    <div className="Card">
+      <div className="card-show-name">
+        <h3>Olá {localStorage.getItem('name')}!</h3>
+      </div>
+
+      <div className="card-container">
+        {
+          dogs.map(el => <h1>{el}</h1>)
+        }
+      </div>
     </div>
   );
 }
